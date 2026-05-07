be4fbd1ea932d2b83a761b30cebf30fc9340abe8
GraceNjonge <88135535+GraceNjonge@users.noreply.github.com>
2026-05-07 12:29:02 +0300

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+     <h1>Indulge in your passions</h1>
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+
+    <div className='navbar'>
+      <h3>EV-olution</h3>
+      <ul> 
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <button>Contact</button>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
